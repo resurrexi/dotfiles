56bdecae19c8ee7ba57236d6b387b9f96b6b3ba3
Liquan Yang <thedevhiro@gmail.com>
2021-11-18 22:17:08 +0800

    exclude vim and lua files from colorizer

--- dot_config/nvim/after/plugin/colorizer.rc.lua
+++ dot_config/nvim/after/plugin/colorizer.rc.lua
@@ -1,6 +1,9 @@
 require 'colorizer'.setup ({
   -- highlight colors of all filetypes
   '*';
+  -- but exclude vim and lua filetypes
+  '!vim';
+  '!lua';
   -- enable parsing rgb(...) functions in css
   css = { rgb_fn = true; };
 }, {
