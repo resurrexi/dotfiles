fef1b38b2100330662771d98bb196133f38c88b4
Liquan Yang <thedevhiro@gmail.com>
2021-11-27 15:56:56 +0800

    add keymap for opening files with fzf

--- dot_config/nvim/lua/config/fzf.lua
+++ dot_config/nvim/lua/config/fzf.lua
@@ -8,3 +8,10 @@ require("fzf-lua").setup({
     }
   }
 })
+
+-- Set keymaps
+local mapper = function(mode, key, result)
+  vim.api.nvim_set_keymap(mode, key, result, {noremap=true, silent=true})
+end
+
+mapper("n", "<leader>o", "<Cmd>FzfLua files<CR>")
