c9b01a65dea969c4c0b8c147f61a9ebfadfc3b8d
Liquan Yang <yang.liquan87@gmail.com>
2022-11-27 11:22:32 -0500

    fix cmp_nvim_lsp deprecation warning

--- dot_config/nvim/lua/config/lspconfig.lua
+++ dot_config/nvim/lua/config/lspconfig.lua
@@ -1,5 +1,5 @@
 local nvim_lsp = require("lspconfig")
-local cmp_capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- Change diagnostic symbols
 local signs = { Error = "■", Warn = "■", Hint = "■", Info = "■" }
