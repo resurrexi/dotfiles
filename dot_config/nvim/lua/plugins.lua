55e0cd5d465328ac514e7cf90682178ee2225558
Liquan Yang <yang.liquan87@gmail.com>
2023-12-27 23:21:18 -0500

    add luasnip

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -94,6 +94,7 @@ require("lazy").setup({
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
+      {"L3MON4D3/LuaSnip", version = "v2.*"},
       "saadparwaiz1/cmp_luasnip",
     },
     config = function()
