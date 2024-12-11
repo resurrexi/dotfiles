d847a69701854cd1f680b92a50f40b2b5af3ba00
Liquan Yang <yang.liquan87@gmail.com>
2024-12-11 16:19:38 -0500

    add db client for nvim

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -198,6 +198,21 @@ require("lazy").setup({
       require("config.zknvim")
     end
   },
+  {
+    "kndndrj/nvim-dbee",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    build = function()
+      -- Install tries to automatically detect the install method.
+      -- if it fails, try calling it with one of these parameters:
+      --    "curl", "wget", "bitsadmin", "go"
+      require("dbee").install()
+    end,
+    config = function()
+      require("dbee").setup()
+    end,
+  },
 
   -- Language support
   {
